37ec69e1a767017fb94f3ddeda51cbeb5758d864
RNViththagan <viththagan1999@gmail.com>
2023-08-19 03:48:42 +0530

    draft login setup for admin and students

---
+++ admin_select.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+print_r($_SESSION);
+include("connect.php");
+$sql = "select role from admin where email = '" . $_SESSION['userid'] . "'";
+
+if ($result = mysqli_query($con, $sql)) {
+    $obj = mysqli_fetch_assoc($result);
+    $_SESSION['role'] = $obj['role'];
+    if ($obj['role'] == "Admin_Master")
+        header("location:master/index.php");
+    else
+        header("location:admin/index.php");
+    exit();
+
+}
+?>
