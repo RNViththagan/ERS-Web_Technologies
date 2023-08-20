59bb0219bb04a9f54ae68fe5605b97166339508d
Viththagan R.N <84705898+RNViththagan@users.noreply.github.com>
2023-08-20 22:07:29 +0530

    Update admin_select.php

--- admin_select.php
+++ admin_select.php
@@ -7,8 +7,10 @@ $sql = "select role from admin where email = '" . $_SESSION['userid'] . "'";
 if ($result = mysqli_query($con, $sql)) {
     $obj = mysqli_fetch_assoc($result);
     $_SESSION['role'] = $obj['role'];
-    if ($obj['role'] == "Admin_Master")
+    if ($obj['role'] == "Admin_Master"){
+        echo "goto master";
         header("location:master/index.php");
+    }
     else
         header("location:admin/index.php");
     exit();
