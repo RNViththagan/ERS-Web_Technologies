2db8781b13a1af29ed2a7de8906e539842ea3e6d
Viththagan R.N <84705898+RNViththagan@users.noreply.github.com>
2023-08-21 12:18:34 +0530

    Update admin_select.php

--- admin_select.php
+++ admin_select.php
@@ -8,12 +8,12 @@ if ($result = mysqli_query($con, $sql)) {
     $obj = mysqli_fetch_assoc($result);
     $_SESSION['role'] = $obj['role'];
     if ($obj['role'] == "Admin_Master"){
-        header("location:master/index.php");
+        header("Location:master/index.php");
         ob_end_flush();
         exit;
     }
     else{
-        header("location:admin/index.php");
+        header("Location:admin/index.php");
         ob_end_flush();
         exit;
     }
