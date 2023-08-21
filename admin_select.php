0a051c90b651fa1a33a8e7c3b52008171c7fcc79
RNViththagan <viththagan1999@gmail.com>
2023-08-21 18:35:51 +0530

    added ob_start() in the begining

--- admin_select.php
+++ admin_select.php
@@ -1,23 +1,20 @@
 <?php
+ob_start();
 session_start();
 print_r($_SESSION);
 include("connect.php");
 $sql = "select role from admin where email = '" . $_SESSION['userid'] . "'";
-ob_start();
 if ($result = mysqli_query($con, $sql)) {
     $obj = mysqli_fetch_assoc($result);
     $_SESSION['role'] = $obj['role'];
     if ($obj['role'] == "Admin_Master"){
-        header("Location:master/index.php");
-        ob_end_flush();
+        header("Location:master");
         exit;
     }
     else{
-        header("Location:admin/index.php");
-        ob_end_flush();
+        header("Location:admin");
         exit;
     }
-    exit();
 
 }
 ?>
