9f7bf3855fc97f205281c98314f95ccbc048b02f
RNViththagan <viththagan1999@gmail.com>
2023-08-26 00:19:37 +0530

    connect file restored db added

--- ers_db.sql
+++ ers_db.sql
@@ -45,7 +45,7 @@ CREATE TABLE `admin` (
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES ('admin_master@nexus.com','$2y$10$9D28If.TzaiX6I3ZoMBVYOuJsMAFu06/8AaGj9rYUvHpx./OmcIO2','Admin_Master','active'),('stud_admin1@nexus.com','$2y$10$IUzrF9GhBdTzDXXbmxA19.XZuxKo9le3hETfrRsqKG35goK4w1npS','Admin_Student','active'),('subj_admin1@nexus.com','$2y$10$6IniUusMCkDLxZFhTVWyL.Nk0BBkFuzzLUzSCdFOqy32NexOPRNvi','Admin_Subject','Active');
+INSERT INTO `admin` VALUES ('admin_master@nexus.com','$2y$10$9D28If.TzaiX6I3ZoMBVYOuJsMAFu06/8AaGj9rYUvHpx./OmcIO2','Admin_Master','active'),('stud_admin1@nexus.com','$2y$10$IUzrF9GhBdTzDXXbmxA19.XZuxKo9le3hETfrRsqKG35goK4w1npS','Admin_Student','active'),('subj_admin1@nexus.com','$2y$10$6IniUusMCkDLxZFhTVWyL.Nk0BBkFuzzLUzSCdFOqy32NexOPRNvi','Admin_Subject','active');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ CREATE TABLE `combination` (
   `combinationID` int(11) NOT NULL AUTO_INCREMENT,
   `combinationName` varchar(50) NOT NULL,
   PRIMARY KEY (`combinationID`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `combination` (
 
 LOCK TABLES `combination` WRITE;
 /*!40000 ALTER TABLE `combination` DISABLE KEYS */;
-INSERT INTO `combination` VALUES (1,'CSC - Direct Intake'),(2,'BOT, ZOO, FSC'),(3,'CHE, BOT, FSC'),(4,'CHE, BOT, ZOO'),(5,'CHE, ZOO, FSC'),(6,'CHE, PMM, AMM'),(7,'CSC, AMM, CHE'),(8,'CSC, AMM, PHY'),(9,'CSC, AMM, STA'),(10,'CSC, PMM, AMM'),(11,'CSC, PMM, CHE'),(12,'CSC, STA, PMM');
+INSERT INTO `combination` VALUES (1,'CSC - Direct Intake'),(2,'BOT, ZOO, FSC'),(3,'CHE, BOT, FSC'),(4,'CHE, BOT, ZOO'),(5,'CHE, ZOO, FSC'),(6,'CHE, PMM, AMM'),(7,'CSC, AMM, CHE'),(8,'CSC, AMM, PHY'),(9,'CSC, AMM, STA'),(10,'CSC, PMM, AMM'),(11,'CSC, PMM, CHE'),(12,'CSC, STA, PMM'),(13,'PHY, PMM, AMM');
 /*!40000 ALTER TABLE `combination` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +96,7 @@ CREATE TABLE `combination_subjects` (
 
 LOCK TABLES `combination_subjects` WRITE;
 /*!40000 ALTER TABLE `combination_subjects` DISABLE KEYS */;
-INSERT INTO `combination_subjects` VALUES (1,'CSC - Direct Intake'),(2,'BOT'),(2,'FSC'),(2,'ZOO'),(3,'BOT'),(3,'CHE'),(3,'FSC'),(4,'CHE'),(5,'CHE'),(6,'CHE'),(7,'CSC'),(8,'CSC'),(9,'AMM'),(9,'CSC'),(9,'STA'),(10,'AMM'),(10,'CSC'),(10,'PMM'),(11,'CHE'),(11,'CSC'),(11,'PMM'),(12,'CSC'),(12,'PMM'),(12,'STA');
+INSERT INTO `combination_subjects` VALUES (1,'CSC - Direct Intake'),(2,'BOT'),(2,'FSC'),(2,'ZOO'),(3,'BOT'),(3,'CHE'),(3,'FSC'),(4,'CHE'),(5,'CHE'),(6,'CHE'),(7,'CSC'),(8,'CSC'),(9,'AMM'),(9,'CSC'),(9,'STA'),(10,'AMM'),(10,'CSC'),(10,'PMM'),(11,'CHE'),(11,'CSC'),(11,'PMM'),(12,'CSC'),(12,'PMM'),(12,'STA'),(13,'AMM'),(13,'PHY'),(13,'PMM');
 /*!40000 ALTER TABLE `combination_subjects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,10 +108,18 @@ DROP TABLE IF EXISTS `student`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `student` (
-  `reg_no` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `status` varchar(100) NOT NULL DEFAULT 'disabled',
-  PRIMARY KEY (`reg_no`)
+  `regNo` varchar(12) NOT NULL,
+  `indexNumber` varchar(10) NOT NULL,
+  `title` varchar(5) NOT NULL,
+  `nameWithIniial` varchar(60) NOT NULL,
+  `fullName` varchar(150) NOT NULL,
+  `district` varchar(30) NOT NULL,
+  `mobileNo` varchar(11) NOT NULL,
+  `landlineNo` varchar(11) NOT NULL,
+  `homeAddress` varchar(300) NOT NULL,
+  `addressInJaffna` varchar(300) NOT NULL,
+  `profile_img` varchar(255) NOT NULL,
+  PRIMARY KEY (`regNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -121,36 +129,36 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES ('2020/CSC/051','viththagan1999@gmail.com','active'),('2020/CSC/074','saaru27kesan@gmail.com','disabled');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `student_details`
+-- Table structure for table `student_check`
 --
 
-DROP TABLE IF EXISTS `student_details`;
+DROP TABLE IF EXISTS `student_check`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `student_details` (
-  `reg_no` varchar(20) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `password` varchar(150) NOT NULL,
-  `otp` varchar(150) NOT NULL,
-  `otp_status` varchar(100) NOT NULL DEFAULT 'none',
-  PRIMARY KEY (`reg_no`),
-  CONSTRAINT `student_details_ibfk_1` FOREIGN KEY (`reg_no`) REFERENCES `student` (`reg_no`) ON UPDATE CASCADE
+CREATE TABLE `student_check` (
+  `regNo` varchar(12) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'unregisterd',
+  `verificationCode` int(6) NOT NULL,
+  `verificationStatus` varchar(15) NOT NULL DEFAULT 'not_verified',
+  PRIMARY KEY (`regNo`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `student_details`
+-- Dumping data for table `student_check`
 --
 
-LOCK TABLES `student_details` WRITE;
-/*!40000 ALTER TABLE `student_details` DISABLE KEYS */;
-INSERT INTO `student_details` VALUES ('2020/CSC/051','','$2y$10$EtWMWC7enKBQqMHRL4LcUOAg/l4VxWH74dF7a5U7fFW.y9rxQy6k6','','verified');
-/*!40000 ALTER TABLE `student_details` ENABLE KEYS */;
+LOCK TABLES `student_check` WRITE;
+/*!40000 ALTER TABLE `student_check` DISABLE KEYS */;
+INSERT INTO `student_check` VALUES ('2020/CSC/051','viththagan1999@gmail.com','$2y$10$r2yaIo09V86NfT8kvmmX3ehl.BHs/ru9u.iFmysz8k0YtKHOSYP1S','active',330432,'verified');
+/*!40000 ALTER TABLE `student_check` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -185,4 +193,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-24  2:27:25
+-- Dump completed on 2023-08-26  0:17:23
