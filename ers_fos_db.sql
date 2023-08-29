596f0b6c135365f43af04dde7fe0094590bfc47a
RNViththagan <viththagan1999@gmail.com>
2023-08-29 13:32:03 +0530

    db updated

--- ers_fos_db.sql
+++ ers_fos_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 29, 2023 at 09:58 AM
+-- Generation Time: Aug 29, 2023 at 10:01 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ers_db`
+-- Database: `ers_fos_db`
 --
 
 -- --------------------------------------------------------
